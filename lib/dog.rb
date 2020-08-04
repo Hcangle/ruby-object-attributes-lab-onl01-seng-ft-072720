fcd6e0afbeec2ea2f328d0afe4611e9ce08a759b
Hannah Angle <hannahcangle@gmail.com>
2020-08-04 17:39:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,2 @@
 class Dog 
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
   
-  def dog_info=(name_breed)
-    name, breed = name_breed.split
-    @name = name
-    @breed = breed
-  end
-  def dog_info
-    "#{@name} #{@breed}".strip
-  end
-
-
-lassie = Dog.new("Lassie")
-lassie.breed = "Collie"
-end
